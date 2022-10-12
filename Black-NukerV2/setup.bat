d87a82b533a7a0b6564dba3c7679fd23ef56fd5d
Duce Boxed U <106915154+DuceBoxedU@users.noreply.github.com>
2022-10-12 20:05:24 +0200

    Add files via upload

---
+++ Black-NukerV2/setup.bat
@@ -0,0 +1,13 @@
+@echo off
+title Black Nuker V2
+echo looking for updates...
+timeout /t 3 /nobreak > NUL
+echo updates found, the version of nuker is v2
+timeout /t 3 /nobreak > NUL
+echo Starting Tool...
+timeout /t 1 /nobreak > NUL
+echo The Nuker is opening, please wait...
+timeout /t 1 /nobreak > NUL
+cls
+python black_nukerV2.py
+pause
